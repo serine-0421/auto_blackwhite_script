6ab85835ba1c7506ae89f1bc690e5eb8da0cfc21
serine-0421 <serine.1984@outlook.com>
2026-05-05 00:54:36 +0800

    重构日志记录器，支持动态名称和调试模式，使用旋转文件处理器

--- auto_blackwhite/utils/logger.py
+++ auto_blackwhite/utils/logger.py
@@ -2,24 +2,47 @@
 
 import logging
 import os
+from logging.handlers import RotatingFileHandler
 from datetime import datetime
 
-def setup_logger(log_dir="logs"):
+
+def setup_logger(
+    name="GameBot",
+    log_dir="logs",
+    debug=False
+):
     os.makedirs(log_dir, exist_ok=True)
-    log_file = os.path.join(log_dir, f"auto_{datetime.now().strftime('%Y%m%d_%H%M%S')}.log")
-    
-    logger = logging.getLogger("GameBot")
-    logger.setLevel(logging.DEBUG)
-    
-    fh = logging.FileHandler(log_file, encoding="utf-8")
-    fh.setLevel(logging.DEBUG)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    fh.setFormatter(formatter)
-    ch.setFormatter(formatter)
-    
-    logger.addHandler(fh)
-    logger.addHandler(ch)
+
+    logger = logging.getLogger(name)
+
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+
+    log_file = os.path.join(log_dir, "game.log")
+
+    file_handler = RotatingFileHandler(
+        log_file,
+        maxBytes=5 * 1024 * 1024,  # 5MB
+        backupCount=5,
+        encoding="utf-8"
+    )
+    file_handler.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG if debug else logging.INFO)
+
+    formatter = logging.Formatter(
+        "[%(asctime)s] [%(levelname)s] "
+        "[%(filename)s:%(lineno)d] "
+        "[%(name)s] %(message)s"
+    )
+
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
     return logger
